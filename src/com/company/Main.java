ed02f2b56194873404f0bae99242baa2a9c8c7be
tyler belmonte <tylerbelmonte@gmail.com>
2021-01-28 19:23:40 -0600

    1st

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i = 8; i >= 2; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000, i));
+        }
+
+        }
+
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount *(interestRate / 100));
+    }
+
+}
